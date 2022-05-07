14ca5e252ee459bc28c87afbb1739259da65d035
unciafidelis <unciafidelis@gmail.com>
2022-05-07 11:59:09 -0500

    Update: testing Telegram Service

--- test/services/TelegramBotService.test.js
+++ test/services/TelegramBotService.test.js
@@ -2,6 +2,7 @@ const TelegramBotService = require("../../lib/services/TelegramBotService");
 
 describe("TelegramBot Class Unit tests", () => {
     test("1) calling undefined to set the test", () => {
-        expect(TelegramBotService.onText()).toBe(undefined)
+        const token = "Not a token"
+        expect(TelegramBotService.onText(token)).toBe(undefined)
     })
 })
