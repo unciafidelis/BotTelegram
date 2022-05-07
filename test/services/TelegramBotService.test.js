a4286a2429918d664331925afbd233b537009581
unciafidelis <unciafidelis@gmail.com>
2022-05-07 18:35:26 -0500

    Add: TelegramBotService unit test passed

--- test/services/TelegramBotService.test.js
+++ test/services/TelegramBotService.test.js
@@ -3,6 +3,6 @@ const TelegramBotService = require("../../lib/services/TelegramBotService");
 describe("TelegramBot Class Unit tests", () => {
     test("1) calling undefined to set the test", () => {
         const token = "Not a token"
-        expect(TelegramBotService.onText(token)).toBe(undefined)
+        expect(TelegramBotService.onMsg(token)).toBe("Token not available")
     })
 })
